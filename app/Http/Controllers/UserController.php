ac0e7fa228ba70d11b7a4bf5c2160e7960bae1e7
aidatulrosida <rosidaaidatul@gmail.com>
2025-03-11 21:34:16 +0700

    JS04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,10 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-{
-    // Tambah data user baru jika belum ada
-    $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'name' => 'Manager 3',
-        'password' => Hash::make('12345')
-    ];
+    {
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
     
-    UserModel::create($data);
-    // Ambil semua data dari tabel m_user
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
-}
+    
+        return view('user', ['data' => [$user]]); // Bungkus dalam array agar tetap bisa di-loop
+    }    
 }
