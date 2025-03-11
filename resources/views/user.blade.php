460d3356fa5d10ffb05d0a154abc7ae43a2fcc8a
aidatulrosida <rosidaaidatul@gmail.com>
2025-03-11 21:34:30 +0700

    JS04

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,21 +5,13 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1" cellPadding="2" cellSpacing="0">
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>Jumlah Pengguna</th>
         </tr>
-        @foreach ($data as $d)
         <tr>
-            <td>{{ $d->user_id }}</td>
-            <td>{{ $d->username }}</td>
-            <td>{{ $d->name }}</td>
-            <td>{{ $d->level_id }}</td>
+            <td>{{ $data }}</td>
         </tr>
-        @endforeach
     </table>
 </body>
 </html>
