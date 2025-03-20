c8c54e82bb7597b61a223d0247e0e5fadda42a55
aidatulrosida <rosidaaidatul@gmail.com>
2025-03-20 18:49:17 +0700

    JS04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,51 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::create([
-            'username' => 'manager56',
-            'name' => 'Manager 56',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+        $data = UserModel::all();
+        return view('user', ['data' => $data]);
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'name' => $request->name, // Pastikan field ini sesuai dengan yang ada di database
+            'password' => Hash::make($request->password),
+            'level_id' => $request->level_id,
         ]);
 
-        $user->username= 'manager12';
+        return redirect('/user'); // Redirect setelah menyimpan
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+        $user->username = $request->username;
+        $user->name = $request->name; // Pastikan field ini sesuai
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
 
         $user->save();
- 
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged('username', 'level_id'); // true   
-        $user->wasChanged('name'); // false
-        dd($user->wasChanged(['name', 'username'])); // true       
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
---
+++ resources/views/user_ubah.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Form Ubah Data User</h1>
+    <a href="/user">Kembali</a>
+    <br><br>
+
+    <form method="post" action="/User/ubah_simpan/{{ $data->user_id }}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        
+        <label>Username</label>
+        <input type="text" name="username" placeholder="Masukan Username" value="{{ $data->username }}">
+        <br>
+
+        <label>Nama</label>
+        <input type="text" name="nama" placeholder="Masukan Nama" value="{{ $data->username }}">
+        <br>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Masukan Password">
+        <br>
+
+        <label>Level ID</label>
+        <input type="text" name="level_id" placeholder="Masukan ID Level" value="{{ $data->level_id }}">
+        <br>
+
+        <input type="submit" class="btn btn-success" value="Ubah">
+    </form>
+</body>
+</html>
